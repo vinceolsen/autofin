61892090622c985c61df56ad648a226923fbbc35
Vince Olsen <volsen@hubspot.com>
2023-08-20 11:19:02 -1000

    add test cases

--- tests/src/test_trading_strategies.py
+++ tests/src/test_trading_strategies.py
@@ -1,12 +1,14 @@
-from ...algofin.src.trading_strategies import BackTest
+from ...algofin.src.trading_strategies import BackTest, Price
 
 
 def test_true():
     assert True
 
+
 def test_it_runs_everything():
     trading_strategies = BackTest()
     assert True
+
 
 def test_it_gets_all_symbols():
     symbols = BackTest().symbols
@@ -17,3 +19,9 @@ def test_it_loads_all_pricing_data():
     assert len(pricing_data) == 2
 
 
+def test_it_loads_a_pricing_file():
+    prices = BackTest().load_csv('RITM')
+    assert len(prices) == 2544
+    assert len(prices[0]) == 6
+    assert prices[0] == Price(symbol='RITM', date='2013-05-02', open=14.0, high=14.0, low=13.0, close=13.52)
+    assert prices[-1] == Price(symbol='RITM', date='2023-06-08', open=8.88, high=8.96, low=8.81, close=8.92)
