1fd78d0cbec99fd6cc6b88163b1fe2a59f3e794c
Vince Olsen <volsen@hubspot.com>
2023-08-20 14:26:24 -1000

    add test case

--- tests/src/test_trading.py
+++ tests/src/test_trading.py
@@ -1,4 +1,5 @@
 from ...algofin.src.trading import BackTest
+from ...algofin.src.objects import Balance
 
 
 def test_true():
@@ -11,3 +12,4 @@ def test_it_finds_the_strategy_with_max_ending_balance():
     strategy_id, ending_balance, balance = back_test.get_max_strategy_ending_balance()
     assert strategy_id == 1
     assert round(ending_balance, 2) == 245631.99
+    assert balance == Balance(strategy_id=1, date='2023-05-01', order_balance=9599.931128899996, cash_balance=188.97569011500232, invested_balance=235843.081464, number_of_shares=732)
