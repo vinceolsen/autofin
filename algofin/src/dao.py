cb54a3fd4c882abdb3f5a839bc2d9cd0e03ffecb
Vince Olsen <volsen@hubspot.com>
2023-08-20 16:23:05 -1000

    add return types

--- algofin/src/dao.py
+++ algofin/src/dao.py
@@ -26,11 +26,11 @@ class Dao:
         return pricing_data
 
     @staticmethod
-    def create_folder_(path):
+    def create_folder_(path) -> None:
         if not os.path.exists(path):
             os.makedirs(path)
 
-    def write_to_csv(self, name: str, rows: list):
+    def write_to_csv(self, name: str, rows: list) -> None:
         path = 'algofin/results/' + self.now
         self.create_folder_(path)
         fullpath = path + '/' + name + '.csv'
@@ -40,20 +40,20 @@ class Dao:
             writer.writerows(rows)
 
     @staticmethod
-    def load_balance(row):
+    def load_balance(row) -> Balance:
         return Balance(int(row[0]), row[1], Decimal(row[2]), Decimal(row[3]), Decimal(row[4]), int(row[5]))
 
     @staticmethod
-    def load_price(row):
+    def load_price(row) -> Price:
         return Price(row[0], row[1], Decimal(row[2]), Decimal(row[3]), Decimal(row[4]), Decimal(row[5]))
 
     @staticmethod
-    def load_strategy(row):
+    def load_strategy(row) -> Strategy:
         return Strategy(int(row[0]), row[1], row[2], Decimal(row[3]), Decimal(row[4]), row[5], int(row[6]),
                         Decimal(row[7]), row[8], row[9], row[10])
 
     @staticmethod
-    def read_csv(path, load_object, skip_headers=False):
+    def read_csv(path, load_object, skip_headers=False) -> []:
         print('reading csv:', path)
 
         with open(path, newline='') as f:
@@ -76,6 +76,6 @@ class Dao:
         path = 'algofin/results/' + self.now + '/strategies.csv'
         return self.read_csv(path, self.load_stategy, False)
 
-    def get_strategy(self, id: int)-> Strategy:
+    def get_strategy(self, id: int) -> Strategy:
         strategies = self.get_strategies()
         return strategies[id]
